e54eb950970c440038feef561e3bc148b777d0f3
Shaan2170 <shaanmustafa1234@gmail.com>
2025-07-26 13:57:36 +0530

    authenticated endpoint

--- index.js
+++ index.js
@@ -55,5 +55,26 @@ app.post("/signin", function (req,res){
 
 })
 
+app.get("/me", function(req,res){
+    const token = req.headers.token
+    const foundUser = null;
+
+    for (let i =0; i<users.length;i++){
+        if(users[i].token == token) {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser){
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    }else {
+        res.json ({
+            message: "token invalid"
+        })
+    }
+})
 
 app.listen(3000);
